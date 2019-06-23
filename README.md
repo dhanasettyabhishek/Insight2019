63fd9ead4407bc44bd7b82d72e05cf12c36ad3af
dhanasettyabhishek <dhanasettyabhishek@gmail.com>
2019-06-23 15:17:14 -0400

    6/23/2019 3:17PM

---
+++ README.md
@@ -0,0 +1,58 @@
+# INSIGHT DATA SCIENCE - Purchase Analytics 
+## Coding Challenge, Abhishek Dhanasetty
+
+Programming language used:
+=================
+
+ > Python 3.7.3
+
+## Table of Contents:
+
+1. Problem Statement
+2. Introduction
+3. Idea 
+4. Testing
+5. Results
+
+### Problem Statement:
+
+> For this challenge, I need to calculate, for each department, the number of times a product was requested, number of times a product was requested for the first time and a ratio of those two numbers.
+
+### Introduction:
+
+I have two input files `order_products.csv`, `products.csv` where,
+
+ `order_products.csv` holds data on one request, and contains the fields, `order_id, product_id, add_to_cart, reordered` amoung which I am only considering product_id and reordered for my analysis. 
+ 
+ `products.csv` holds information about every product, and contains the fields, `product_id, product_name, aisle_id, department_id` and in this input file I am only considering product_id and department_id`.
+ 
+From the given input dataset, I am calculating the number of product requested and number of product requested for the first time.
+
+### Idea:
+
+1. First I am reading the products.csv file and I am creating a dictionary with `key: product_id` and `value:department_id`.
+	> For the above dataset, the dictionary is:
+		{9327: 13, 17461: 12, 17668: 16, 28985: 4, 32665: 3, 33120: 16, 45918: 13, 46667: 4, 46842: 3}
+
+2. Then I am reading the order_products.csv file and I am creating a dictionary with `key: product_id -> department_id` (from the previously created dictionary and the `value: [reordered information]`.
+
+	> {16: [1, 1], 4: [1, 1], 13: [0, 1], 12: [1], 3: [1, 0]}
+
+3. Finally, I am storing the values such as department_id,number_of_orders,number_of_first_orders,percentage in report.csv.
+	> Sample output:
+		
+		![output](./insight_testsuite/images/1.png)
+		
+
+### Testing
+
+1. Testing the code using the given test suite.
+
+    ![Test Suite 1 & 2](./insight_testsuite/images/2.png)
+
+### Results
+
+The results are stored in the output folder as report.csv
+
+
+Thank you.
--- insight_testsuite/temp/src/prediction-validation.py
+++ insight_testsuite/temp/src/prediction-validation.py
@@ -9,6 +9,11 @@ class Purchase_Analytics(object):
     def main(self):    
         prod_dict = dict()
         try:
+            """
+            Creating a dictionary to store product_id and department_id information.
+            key: product_id
+            value: department_id
+            """
             with open("./input/products.csv", mode = "r", encoding="utf8") as products:        
                 next(products)
                 for j in products:
@@ -16,12 +21,18 @@ class Purchase_Analytics(object):
                     if int(j[0]) not in prod_dict:
                         prod_dict[int(j[0])] = int(j[-1])
             products.close()
+        # If products.csv file doesnot exists.
         except FileNotFoundError:
             print("products.csv file doesnot exists")
             return
 
         op_pid_reord = dict()
         try:
+            """
+            Creating a dictionary to store department_id and reordered details.
+            key: department_id
+            value: list(reordered products)
+            """
             with open("./input/order_products.csv", mode = "r", encoding="utf8") as order_products:
                 next(order_products)
                 for i in order_products:
@@ -32,14 +43,16 @@ class Purchase_Analytics(object):
                     else:
                         op_pid_reord[prod_dict[int(i[1])]].append(int(i[-1]))
             order_products.close()
+        # If order_products.csv file doesnot exists
         except FileNotFoundError:
             print("order_products.csv file doesnot exists")
             return
-
+        """
+        Storing the required information and calculating the number of requests per product.
+        """
         output = open("./output/report.csv", mode = "w", encoding="utf8")
         output.write("department_id,number_of_orders,number_of_first_orders,percentage\n")
         for key, value in sorted(op_pid_reord.items(), key = lambda x: x[0]):
-#            print("{},{},{},{}\n".format(str(key), str(len(value)), str(len(value)-sum(value)), "{:.2f}".format(float(len(value)-sum(value))/float(len(value)))))
             output.write("{},{},{},{}\n".format(str(key), str(len(value)), str(len(value)-sum(value)), "{:.2f}".format(float(len(value)-sum(value))/float(len(value)))))
         output.close()
         
