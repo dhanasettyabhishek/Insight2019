018228691ea14d4fb8caf01760f94b446e392ea8
Abhishek Dhanasetty <dhanasettyabhishek@gmail.com>
2019-06-30 19:20:33 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python333 ./src/prediction-validation.py ./input/products.csv ./input/order_products.csv ./output/report.csv
+python3 ./src/prediction-validation.py ./input/products.csv ./input/order_products.csv ./output/report.csv
